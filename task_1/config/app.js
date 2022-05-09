2c75bcc1a5a58857eca99a902c8cfe84fed032ee
Petr Srek <petr.srek@gmail.com>
2022-05-09 16:59:41 +0200

    fix config

--- task_1/config/app.js
+++ task_1/config/app.js
@@ -14,8 +14,6 @@ dotevn.config(options)
 export default {
     logFilePath: process.env.LOG_FILE || './storage/log.txt',
     port: process.env.PORT || 5000,
-    appUrl: process.env.APP_URL || 'http://localhost:5000',
-
 
     redisPort: process.env.REDIS_PORT || 6397,
     redisHost: process.env.REDIS_HOST || 'localhost',
